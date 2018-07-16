a1b3c1164ed009db40bb02953197ef4a41188fa3
SI9INT <38978231+si9int@users.noreply.github.com>
2018-07-16 11:45:56 +0200

    Update: v.0.2
    + Specify an output file
    + Specify a year to crawl for
    + 65% faster

--- cc.py
+++ cc.py
@@ -2,50 +2,83 @@ import requests, json
 import argparse
 
 parser = argparse.ArgumentParser()
+
 parser.add_argument('domain', help = 'domain which will be crawled for', type = str)
+parser.add_argument('-y', '--year', help = 'limit the result to a specific year (default: all)', type = str)
+parser.add_argument('-o', '--out', help = 'specify an output file (default: domain.txt)', type = str)
 
 args = parser.parse_args()
-
-indexes = [
-	'CC-MAIN-2018-17',
-	'CC-MAIN-2018-13',
-	'CC-MAIN-2018-09',
-	'CC-MAIN-2018-05',
-	'CC-MAIN-2017-51',
-	'CC-MAIN-2017-47',
-	'CC-MAIN-2017-43',
-	'CC-MAIN-2017-39',
-	'CC-MAIN-2017-34',
-	'CC-MAIN-2017-30',
-	'CC-MAIN-2017-26',
-	'CC-MAIN-2017-22',
-	'CC-MAIN-2017-17',
-	'CC-MAIN-2017-13',
-	'CC-MAIN-2017-09',
-	'CC-MAIN-2017-04'
-]
-
-output = []
-result = open('./' + args.domain + '.txt', 'w')
 links = []
+out = ''
 
+if args.out:
+	result = open('./' + args.out, 'w')
+	output = str(args.out)
+else:
+	result = open('./' + args.domain + '.txt', 'w')
+	output = str(args.domain + '.txt')
 
-for index in indexes:
-	print('[-] Getting: ' + index)
+indexes = {
+	'y18' : [
+		'CC-MAIN-2018-17',
+		'CC-MAIN-2018-13',
+		'CC-MAIN-2018-09',
+		'CC-MAIN-2018-05'
+	],
+	'y17' : [
+		'CC-MAIN-2017-51',
+		'CC-MAIN-2017-47',
+		'CC-MAIN-2017-43',
+		'CC-MAIN-2017-39',
+		'CC-MAIN-2017-34',
+		'CC-MAIN-2017-30',
+		'CC-MAIN-2017-26',
+		'CC-MAIN-2017-22',
+		'CC-MAIN-2017-17',
+		'CC-MAIN-2017-13',
+		'CC-MAIN-2017-09',
+		'CC-MAIN-2017-04'
+	]
+}
+
+def getData(index):
+	global links
+	global out
+
 	data = requests.get('http://index.commoncrawl.org/' + index + '-index?url=*.' + args.domain + '&output=json')
 	data = data.text.split('\n')[:-1]
-	output.append(data)
 
-for entry in output:
-	for link in entry:
-		link = json.loads(link)['url']
+	for entry in data:
+		link = json.loads(entry)['url']
 
 		if link not in links:
-			links.append(link + '\n')
+			out = out + (link + '\n')
 
-print('[-] Writing URLS to file')
 
-for link in links:
-	result.write(link)
+def crawlAll():
+	for year, index in indexes.items():
+		print('[!] Proceeding year: 20' + year[1:])
 
-print('[!] Done, file written: ./' + args.domain + '.txt')
+		for i in index:
+			print('[-] ' + i)
+			getData(i)
+
+
+def crawlSpecific(year):
+	index = indexes.get('y' + year)
+	print('[!] Proceeding year: 20' + year)
+
+	for i in index:
+		print('[-] ' + i)
+		getData(i)
+
+
+if args.year:
+	crawlSpecific(args.year)
+else:
+	crawlAll()
+
+print('[-] Writing to file ...')
+result.write(out)
+
+print('[!] Done, file written: ./' + output)
