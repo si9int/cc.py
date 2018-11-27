acd32a83fce2456f7f2709a6589ac89e0af86124
iontodd <iontodd@aaathats3as.com>
2018-11-27 15:05:42 +0200

    Moved indexes to file, added --update to re-pull indexes, changed year from 2 to 4 digits (never forget y2k)

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Extracting URLs of a specific target based on the results of "commoncrawl.org".
 Updated to v.0.3 | Whats new:
 
 - 65% faster proceeding
-- Specify a year via `-y/--year`, e.g.: `-y 18`
+- Specify a year via `-y/--year`, e.g.: `-y 2018`
 - Specify an output file via `-o/--out`, e.g.: `-o whatever.txt`
 - Crawl all pages for a specific index `-i/--index`, e.g.: `cc.py army.mil -i CC-MAIN-2018-05`
 - List all available indexes `-l/--list`, e.g.: `cc.py army.mil -l`
@@ -19,7 +19,7 @@ Updated to v.0.3 | Whats new:
 
 **Usage**
 ```
-cc.py [-h] [-y YEAR] [-o OUT] [-l] [-i INDEX] domain
+cc.py [-h] [-y YEAR] [-o OUT] [-l] [-i INDEX] [-u] domain
 
 positional arguments:
   domain                domain which will be crawled for
@@ -32,11 +32,13 @@ optional arguments:
   -i INDEX, --index INDEX
                         Crawl for a specific index (this will crawl all
                         pages!)
+  -u, --update          Update index file
+
 ```
 
 **Example**
 ```
-python3 cc.py github.com -y 18 -o github_18.txt
+python3 cc.py github.com -y 2018 -o github_18.txt
 cat github_18.txt | grep user
 ```
 
--- cc.py
+++ cc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import requests, json, os
-import argparse
+import argparse, datetime, pathlib
 
 parser = argparse.ArgumentParser()
 
@@ -9,33 +9,13 @@ parser.add_argument('-y', '--year', help = 'limit the result to a specific year 
 parser.add_argument('-o', '--out', help = 'specify an output file (default: domain.txt)', type = str)
 parser.add_argument('-l', '--list', help = 'Lists all available indexes', action = 'store_true')
 parser.add_argument('-i', '--index', help = 'Crawl for a specific index (this will crawl all pages!)', type = str)
+parser.add_argument('-u', '--update', help = 'Update index file', action = 'store_true')
 
 args = parser.parse_args()
 links = []
 out = ''
 
-indexes = {
-	'y18' : [
-		'CC-MAIN-2018-17',
-		'CC-MAIN-2018-13',
-		'CC-MAIN-2018-09',
-		'CC-MAIN-2018-05'
-	],
-	'y17' : [
-		'CC-MAIN-2017-51',
-		'CC-MAIN-2017-47',
-		'CC-MAIN-2017-43',
-		'CC-MAIN-2017-39',
-		'CC-MAIN-2017-34',
-		'CC-MAIN-2017-30',
-		'CC-MAIN-2017-26',
-		'CC-MAIN-2017-22',
-		'CC-MAIN-2017-17',
-		'CC-MAIN-2017-13',
-		'CC-MAIN-2017-09',
-		'CC-MAIN-2017-04'
-	]
-}
+indexes = []
 
 def getData(index, page):
 	global links
@@ -52,44 +32,74 @@ def getData(index, page):
 
 
 def crawlAll():
-	for year, index in indexes.items():
-		print('[!] Proceeding year: 20' + year[1:])
+	#Below code assumes that array is sorted
+	#Reverse sort ensures that most recent years are prioritised, not essential. 
+	currentyear=0
+	indexes.sort(reverse=1)
+	
+	for index in indexes:
+		if currentyear != index.split("-")[2]:
+			currentyear = index.split("-")[2]
+			print("[!] Processing year: " + currentyear)
+		print('[-] ' + index)
+		getData(index, '')
 
-		for i in index:
-			print('[-] ' + i)
-			getData(i, '')
-
-
+#
 def crawlSpecific(year):
-	index = indexes.get('y' + year)
-	print('[!] Proceeding year: 20' + year)
+	#index = indexes.get('y' + year)
+	print('[!] Processing year: ' + year)
 
-	for i in index:
-		print('[-] ' + i)
-		getData(i, '')
+	for index in indexes:
+		if year in index:
+			print('[-] ' + index)
+			getData(index, '')
 
 
 def crawlIndex(domain, index):
 	url = 'http://index.commoncrawl.org/' + index + '-index?url=*.' + domain + '&output=json&showNumPages=true'
 	data = requests.get(url).text
-
 	try:
 		pages = json.loads(data)['pages']
 		print('[-] Collected ' + str(pages) + ' pages')
 	
 		for i in range(0, pages):
 			getData(index, str(i))
-			print('[-] Proceeded page #' + str(i))
+			print('[-] Processing page #' + str(i))
 			
 	except:
 		print('[!] Error reading index')
 		pass
 
+def readIndexFile(index_filename="index.txt"):
+	global indexes
+	#check when the index file was last updated
+	path = pathlib.Path(index_filename)
+	last_updated=datetime.datetime.fromtimestamp(path.stat().st_mtime).strftime('%Y-%m-%d')
+	print("Index file last updated on:", last_updated, "run with -u to update.")
+
+	with open(index_filename, "r") as f:
+		indexes = f.read().split('\n')[:-1]
+
+def updateIndexFile():
+	with open("index.txt", "w") as f:
+		url = "https://index.commoncrawl.org/collinfo.json"
+		data = requests.get(url).text
+		raw_indexes = json.loads(data)
+		for index in raw_indexes:
+			indexes.append(index['id'])
+			f.write(index['id']+"\n")
+
+
+#check if we need to update the index file or just read it into the array
+
+if args.update:
+	updateIndexFile()
+else:
+	readIndexFile()
 
 if args.list:
-	for year, index in indexes.items():
-		for i in index:
-			print('[-] ' + i)
+	for index in indexes:
+		print('[-] ' + index)	
 else:
 	if args.index:
 		crawlIndex(args.domain, args.index)
